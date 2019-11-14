ea304c781505825534fd97d4f680374e359cc5cb
Bolaji Bello <moborlergie1905@gmail.com>
2019-11-14 18:24:37 +0100

    Initial Commit

---
+++ IdentityServerDemo.api/Data/BankContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServerDemo.api.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace IdentityServerDemo.api.Data
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
