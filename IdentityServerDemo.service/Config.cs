79ff428c66802bfdeb85fa9118c45868187c7746
Dennis247 <denghazi619@gmail.com>
2019-11-15 08:34:33 +0100

    IdentityServer Added

---
+++ IdentityServerDemo.service/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerDemo.service
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>{
+                new ApiResource("IdentityServerdemo.api","Customer APi for Identity")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client{
+                 ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                AllowedScopes = { "IdentityServerdemo.api" }
+                
+                },
+              
+            };
+        }
+    }
+}
